c514cac76a747e1eb54bd42c905104afdb631d7d
germanespinosa <germanespinosa@gmail.com>
2020-01-15 14:19:30 -0600

    modified:   Resources/catchtests.cpp

--- Resources/catchtests.cpp
+++ Resources/catchtests.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main( int argc, char* argv[] ) {
-    cout << endl << "Catch 2.0 Automatic CMAKE integration by German Espinosa v1.0.2020 germanespinosa@gmail.com" << endl;
+    cout << endl << "Catch 2.0 Automatic CMAKE integration v1.0.2020" << endl << "by German Espinosa (germanespinosa@gmail.com)" << endl;
     int result = Catch::Session().run( argc, argv );
     return result;
 }
