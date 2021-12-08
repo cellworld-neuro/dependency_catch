1d700ac55172e3f95a7abd8352714f637af6451e
German Espinosa <germanespinosa@gmail.com>
2021-12-08 11:52:37 -0600

    prevents testing for dependencies

--- Resources/CatchTestsConfig.cmake
+++ Resources/CatchTestsConfig.cmake
@@ -3,30 +3,31 @@ set(CATCH_TEST_FOLDER_SOURCE ${CMAKE_CURRENT_LIST_DIR} CACHE PATH "")
 message("CATCH_TESTS CONFIGURATION - $ENV{CATCH_TESTS} -")
 
 if ("$ENV{CATCH_TESTS}" MATCHES "NO_TESTS")
-    MESSAGE("NO TESTS")
+    function (test_library)
+    endfunction()
+else()
+    function (test_library)
+        set (libraries ${ARGN})
+
+        file(GLOB ${PROJECT_NAME}_tests_glob
+                "catchtests/*.h"
+                "catchtests/*.cpp"
+                )
+
+        add_executable(${PROJECT_NAME}_tests ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${${PROJECT_NAME}_tests_glob})
+
+        set_target_properties(${PROJECT_NAME}_tests
+                PROPERTIES
+                RUNTIME_OUTPUT_DIRECTORY ".tests")
+
+        target_link_libraries(${PROJECT_NAME}_tests ${libraries})
+
+        add_test(${PROJECT_NAME}Tests .tests/${PROJECT_NAME}_tests)
+
+        if ("${CMAKE_BUILD_TYPE}" EQUAL "Release")
+            add_custom_command(TARGET ${PROJECT_NAME}_tests
+                    POST_BUILD
+                    COMMAND .tests/${PROJECT_NAME}_tests )
+        endif()
+    endfunction()
 endif()
-
-function (test_library)
-    set (libraries ${ARGN})
-
-    file(GLOB ${PROJECT_NAME}_tests_glob
-            "catchtests/*.h"
-            "catchtests/*.cpp"
-            )
-
-    add_executable(${PROJECT_NAME}_tests ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${${PROJECT_NAME}_tests_glob})
-
-    set_target_properties(${PROJECT_NAME}_tests
-            PROPERTIES
-            RUNTIME_OUTPUT_DIRECTORY ".tests")
-
-    target_link_libraries(${PROJECT_NAME}_tests ${libraries})
-
-    add_test(${PROJECT_NAME}Tests .tests/${PROJECT_NAME}_tests)
-
-    if ("${CMAKE_BUILD_TYPE}" EQUAL "Release")
-        add_custom_command(TARGET ${PROJECT_NAME}_tests
-                POST_BUILD
-                COMMAND .tests/${PROJECT_NAME}_tests )
-    endif()
-endfunction()
