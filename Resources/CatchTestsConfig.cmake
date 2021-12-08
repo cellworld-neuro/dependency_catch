dd8f040743e8bd5a2a1375f785c35b1ef0f80598
German Espinosa <germanespinosa@gmail.com>
2021-12-08 11:03:39 -0600

    prevents testing for dependencies

--- Resources/CatchTestsConfig.cmake
+++ Resources/CatchTestsConfig.cmake
@@ -1,26 +1,28 @@
 set(CATCH_TEST_FOLDER_SOURCE ${CMAKE_CURRENT_LIST_DIR} CACHE PATH "")
 
 function (test_library)
-    set (libraries ${ARGN})
+    if (NOT ${CATCH_TEST} EQUAL "NO_TEST")
+        set (libraries ${ARGN})
 
-    file(GLOB ${PROJECT_NAME}_tests_glob
-        "catchtests/*.h"
-        "catchtests/*.cpp"
-    )
+        file(GLOB ${PROJECT_NAME}_tests_glob
+            "catchtests/*.h"
+            "catchtests/*.cpp"
+        )
 
-    add_executable(${PROJECT_NAME}_tests ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${${PROJECT_NAME}_tests_glob})
+        add_executable(${PROJECT_NAME}_tests ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${${PROJECT_NAME}_tests_glob})
 
-    set_target_properties(${PROJECT_NAME}_tests
-        PROPERTIES
-        RUNTIME_OUTPUT_DIRECTORY ".tests")
+        set_target_properties(${PROJECT_NAME}_tests
+            PROPERTIES
+            RUNTIME_OUTPUT_DIRECTORY ".tests")
 
-    target_link_libraries(${PROJECT_NAME}_tests ${libraries})
+        target_link_libraries(${PROJECT_NAME}_tests ${libraries})
 
-    add_test(${PROJECT_NAME}Tests .tests/${PROJECT_NAME}_tests)
+        add_test(${PROJECT_NAME}Tests .tests/${PROJECT_NAME}_tests)
 
-    if (CMAKE_BUILD_TYPE MATCHES Release)
-        add_custom_command(TARGET ${PROJECT_NAME}_tests
-                POST_BUILD
-                COMMAND .tests/${PROJECT_NAME}_tests )
+        if (${CMAKE_BUILD_TYPE} EQUAL "Release")
+            add_custom_command(TARGET ${PROJECT_NAME}_tests
+                    POST_BUILD
+                    COMMAND .tests/${PROJECT_NAME}_tests )
+        endif()
     endif()
 endfunction()
