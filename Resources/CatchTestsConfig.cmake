6020a81a414b75097d8f84b9c671b1c35756cfeb
German Espinosa <germanespinosa@gmail.com>
2022-01-08 17:51:03 -0600

    allows specifying the testing files

--- Resources/CatchTestsConfig.cmake
+++ Resources/CatchTestsConfig.cmake
@@ -5,38 +5,39 @@ if ("$ENV{CATCH_TESTS}" MATCHES "NO_TESTS")
     endmacro()
 else()
     enable_testing()
-    macro (test_library library)
+    macro(test_library library)
         if ("${ARGN}" STREQUAL "")
-            file(GLOB tests_files
+            file(GLOB CATCH_TEST_FILES
                     "catchtests/*.h"
                     "catchtests/*.cpp"
                     )
         else()
-            set (test_files ${ARGN})
-
+            set (CATCH_TEST_FILES ${ARGN})
         endif()
         string(LENGTH ${CATCH_TEST_COUNTER} COUNTER_LEN)
         MATH(EXPR CATCH_TEST_COUNTER_START "2-${COUNTER_LEN}")
         string(SUBSTRING "00${CATCH_TEST_COUNTER}" ${CATCH_TEST_COUNTER_START} 2 CATCH_TEST_COUNTER_STR)
-        set (test_name ${PROJECT_NAME}_${CATCH_TEST_COUNTER_STR}_tests)
-        add_executable(${test_name} ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${tests_files})
+        set (CATCH_TEST_NAME ${PROJECT_NAME}_${CATCH_TEST_COUNTER_STR}_tests)
+        add_executable(${CATCH_TEST_NAME} ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${CATCH_TEST_FILES})
 
-        set_target_properties(${test_name}
+        set_target_properties(${CATCH_TEST_NAME}
                 PROPERTIES
                 RUNTIME_OUTPUT_DIRECTORY ".tests")
 
-        target_link_libraries(${test_name} ${library})
+        target_link_libraries(${CATCH_TEST_NAME} ${library})
 
-        add_test(${PROJECT_NAME}Tests .tests/${test_name})
+        add_test(${PROJECT_NAME}Tests .tests/${CATCH_TEST_NAME})
         if ("${CMAKE_BUILD_TYPE}" MATCHES "Release")
-            add_custom_command(TARGET ${test_name}
+            add_custom_command(TARGET ${CATCH_TEST_NAME}
                     POST_BUILD
-                    COMMAND .tests/${test_name} )
+                    COMMAND .tests/${CATCH_TEST_NAME} )
         endif()
         MATH(EXPR CATCH_TEST_COUNTER "${CATCH_TEST_COUNTER}+1")
     endmacro()
 
     macro(test_files)
+        set(test_files "")
+        set(source_files "")
         message(STATUS "\nARGN: ${ARGN}\n")
         foreach(arg IN ITEMS ${ARGN})
             message(STATUS "\n ARG : ${arg} \n")
