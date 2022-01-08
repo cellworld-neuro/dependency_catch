ff820bd465b24dc79e58372e06bb00a4490b623f
German Espinosa <germanespinosa@gmail.com>
2022-01-08 17:00:39 -0600

    allows specifying the testing files

--- Resources/CatchTestsConfig.cmake
+++ Resources/CatchTestsConfig.cmake
@@ -5,21 +5,22 @@ if ("$ENV{CATCH_TESTS}" MATCHES "NO_TESTS")
     endfunction()
 else()
     enable_testing()
-    function (test_library)
-        set (libraries ${ARGN})
-
-        file(GLOB ${PROJECT_NAME}_tests_glob
-                "catchtests/*.h"
-                "catchtests/*.cpp"
-                )
-
-        add_executable(${PROJECT_NAME}_tests ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${${PROJECT_NAME}_tests_glob})
+    function (test_library library)
+        if (${ARGN} STREQUAL "")
+            file(GLOB tests_files
+                    "catchtests/*.h"
+                    "catchtests/*.cpp"
+                    )
+        else()
+            set (test_files ${ARGN})
+        endif()
+        add_executable(${PROJECT_NAME}_tests ${CATCH_TEST_FOLDER_SOURCE}/catchtests.cpp ${tests_files})
 
         set_target_properties(${PROJECT_NAME}_tests
                 PROPERTIES
                 RUNTIME_OUTPUT_DIRECTORY ".tests")
 
-        target_link_libraries(${PROJECT_NAME}_tests ${libraries})
+        target_link_libraries(${PROJECT_NAME}_tests ${library})
 
         add_test(${PROJECT_NAME}Tests .tests/${PROJECT_NAME}_tests)
         if ("${CMAKE_BUILD_TYPE}" MATCHES "Release")
